803762dbef666f307fc6efe3b94a7ac1f66bf074
llopezsantiago <llopez.santhiago@gmail.com>
2026-02-08 17:58:23 -0400

    Entorno Python listo

--- Analisis_Python.py
+++ Analisis_Python.py
@@ -1,2 +1,13 @@
-hola = 1
-print(hola)
+import pandas as pd
+import numpy as np
+
+# Creamos unos datos de prueba
+data = {
+    'Lenguaje': ['Python', 'R'],
+    'Estado': ['Configurado', 'Configurado'],
+    'Nivel': [10, 10]
+}
+
+df = pd.DataFrame(data)
+print("¡Felicidades! Tu entorno de Ciencia de Datos está listo:")
+print(df)
